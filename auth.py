5ba17dfd1f2ff367958dd645fcbcfaa6305cb9ad
Vasiliy Kolmachevskiy <42946191+Waso-python@users.noreply.github.com>
2022-02-27 16:38:20 +0300

    comm

--- auth.py
+++ auth.py
@@ -1,20 +1,21 @@
 import requests
 from requests.auth import HTTPBasicAuth
 
-
+login = ''
+password = ''
 pages = []
 s = requests.session()
 headers = {'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'}
 # s.headers.update(headers)
 
-req = requests.get('https://green-spark.ru/personal/orders/?per_page=100', auth=HTTPBasicAuth('', ''))
+req = requests.get('https://green-spark.ru/personal/orders/?per_page=100', auth=HTTPBasicAuth(login, password))
 pages.append(req.text)
-req = requests.get('https://green-spark.ru/personal/orders/?PAGEN_1=2&per_page=100', auth=HTTPBasicAuth('ks.gulk@gmail.com', 'Dfvgbhs1'))
+req = requests.get('https://green-spark.ru/personal/orders/?PAGEN_1=2&per_page=100', auth=HTTPBasicAuth(login, password))
 pages.append(req.text)
-req = requests.get('https://green-spark.ru/personal/orders/?PAGEN_1=3&per_page=100', auth=HTTPBasicAuth('ks.gulk@gmail.com', 'Dfvgbhs1'))
+req = requests.get('https://green-spark.ru/personal/orders/?PAGEN_1=3&per_page=100', auth=HTTPBasicAuth(login, password))
 pages.append(req.text)
-req = requests.get('https://green-spark.ru/personal/orders/?PAGEN_1=4&per_page=100', auth=HTTPBasicAuth('ks.gulk@gmail.com', 'Dfvgbhs1'))
+req = requests.get('https://green-spark.ru/personal/orders/?PAGEN_1=4&per_page=100', auth=HTTPBasicAuth(login, password))
 pages.append(req.text)
-req = requests.get('https://green-spark.ru/personal/orders/?PAGEN_1=5&per_page=100', auth=HTTPBasicAuth('ks.gulk@gmail.com', 'Dfvgbhs1'))
+req = requests.get('https://green-spark.ru/personal/orders/?PAGEN_1=5&per_page=100', auth=HTTPBasicAuth(login, password))
 pages.append(req.text)
 
